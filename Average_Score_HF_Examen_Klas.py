3a5d11cd44b893710158184a46b30482e8e9513f
Denferno <80751954+Denferno@users.noreply.github.com>
2022-04-08 14:35:06 +0200

    Update Average_Score_HF_Examen_Klas.py

--- Average_Score_HF_Examen_Klas.py
+++ Average_Score_HF_Examen_Klas.py
@@ -7,15 +7,15 @@ site = 'https://leerlingenhf.trinitascollege.nl/Portaal/Persoonlijke_info/Examen
 
 logininfo = {
     'wu_loginname': school_creds.username,
-    'wu_password': school_creds.password 
+    'wu_password' : school_creds.password 
 }
 
 def main():
     global grades_lst
-    grades_lst = grades() # cijfers achterhalen
-    tekortpunten = tekort_compensatie_punten()[0] # tekortpunten optellen
-    compensatiepunten = tekort_compensatie_punten()[1] # compensatie punten optellen
-    gemiddelde = sum(grades_lst) / len(grades_lst) # average of all my grades
+    grades_lst = grades()  # cijfers achterhalen
+    tekortpunten = tekort_compensatie_punten()[0]  # tekortpunten optellen
+    compensatiepunten = tekort_compensatie_punten()[1]  # compensatie punten optellen
+    gemiddelde = sum(grades_lst) / len(grades_lst)  # average of all my grades
     print(f'Je staat een {gemiddelde} gemiddeld')
     print(f'Je hebt {tekortpunten} tekortpunten')
     print(f'Je hebt {compensatiepunten} compensatiepunten')
@@ -28,13 +28,13 @@ def grades():
         response = s.get(site)
         soup = BeautifulSoup(response.content, 'html.parser')
         for number in range(1, 13): 
-            if number == 3 or number == 4 or number == 5 or number == 7: # skipping these numbers because these are parts of the 'combi cijfer'
+            if number == 3 or number == 4 or number == 5 or number == 7:  # skipping these numbers because these are parts of the 'combi cijfer'
                 continue
             else:
                 grade = soup.select(f'tr:nth-child({number}) td:nth-child(3) span a span')
-                grade = grade[0].text # Instead of [<span>8</span>] if will say 8
+                grade = grade[0].text  # Instead of [<span>8</span>] it will say 8
                 grades_lst.append(grade)
-        grades_lst = [float(string) for string in grades_lst] # float will make the numbers and not strings so that I can use sum()
+        grades_lst = [float(string) for string in grades_lst]  # float will make the numbers and not strings so that I can use sum()
         return grades_lst
     
 def tekort_compensatie_punten():
